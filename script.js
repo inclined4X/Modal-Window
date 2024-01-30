c3a8990a310d050e0f6b88fcbd3a57f73796a44b
inclined4X <jezemiahsam48@gmail.com>
2024-01-30 22:04:03 +0000

    made the code dry and worked with classes

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-    //The above can also be done by
-    // modal.style.display = 'block';
-  });
-}
-
-btnCloseModal.addEventListener('click', function () {
+const closedModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+  console.log('button clicked');
+
+  //The above can also be done by
+  // modal.style.display = 'block';
+}
+
+btnCloseModal.addEventListener('click', closedModal);
+overlay.addEventListener('click', closedModal);
